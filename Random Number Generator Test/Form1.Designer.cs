daf7d2141795ce538f368dd5f4aded4b90fcf335
Diord <Diord@DESKTOP-6B09NPQ>
2019-03-29 23:18:06 +0300

    Add project files.

---
+++ Random Number Generator Test/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Random_Number_Generator_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.first = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.second = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.third = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.first,
+            this.second,
+            this.third});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(370, 531);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // first
+            // 
+            this.first.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.first.HeaderText = "1-9";
+            this.first.MaxInputLength = 9;
+            this.first.Name = "first";
+            this.first.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.first.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // second
+            // 
+            this.second.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.second.HeaderText = "10-99";
+            this.second.MaxInputLength = 99;
+            this.second.Name = "second";
+            this.second.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.second.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // third
+            // 
+            this.third.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.third.HeaderText = "100-999";
+            this.third.MaxInputLength = 999;
+            this.third.Name = "third";
+            this.third.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.third.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dataGridView2.Location = new System.Drawing.Point(376, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(370, 531);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn1.HeaderText = "1-9";
+            this.dataGridViewTextBoxColumn1.MaxInputLength = 9;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewTextBoxColumn1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn2.HeaderText = "10-99";
+            this.dataGridViewTextBoxColumn2.MaxInputLength = 99;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewTextBoxColumn2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn3.HeaderText = "100-999";
+            this.dataGridViewTextBoxColumn3.MaxInputLength = 999;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewTextBoxColumn3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 534);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(524, 534);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 557);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(564, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(671, 557);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 26);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 560);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 595);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn first;
+        private System.Windows.Forms.DataGridViewTextBoxColumn second;
+        private System.Windows.Forms.DataGridViewTextBoxColumn third;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Random Number Generator Test/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+
+namespace Random_Number_Generator_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = "random_number_table.txt";
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    dataGridView1.Rows.Add(GetNextNumber(sr),
+                                            GetNextNumber(sr)+ GetNextNumber(sr),
+                                            GetNextNumber(sr)+ GetNextNumber(sr) + GetNextNumber(sr));
+                }
+            }
+
+            SerialPort myPort = new SerialPort("COM3", 115200);
+            myPort.Open();
+
+            for (int i = 0, j = 0; i < 10; i++, j = j + 6)
+            {
+                dataGridView2.Rows.Add(GetNumberFromArduino(myPort),
+                                        GetNumberFromArduino(myPort) + GetNumberFromArduino(myPort),
+                                        GetNumberFromArduino(myPort) + GetNumberFromArduino(myPort) + GetNumberFromArduino(myPort));
+            }
+            float o = 0, l = 0;
+            for (int i = 0; i < tableBinarySequance.Length; i++)
+            {
+                if (tableBinarySequance[i] == '0')
+                    o++;
+                else if (tableBinarySequance[i] == '1')
+                    l++;
+            }
+            label1.Text = (o / l).ToString();
+            o = 0;
+            l = 0;
+            for (int i = 0; i < arduinoBinarySequance.Length; i++)
+            {
+                if (arduinoBinarySequance[i] == '0')
+                    o++;
+                else if (arduinoBinarySequance[i] == '1')
+                    l++;
+            }
+            label2.Text = (o / l).ToString();
+        }
+        public static string arduinoBinarySequance;
+        public static string tableBinarySequance;
+        private static string GetNumberFromArduino(SerialPort sp)
+        {
+            string st;
+            int result;
+            int i = 0;
+            while (true)
+            {
+                if ((st = char.ConvertFromUtf32(sp.ReadChar())) != "?")
+                {
+                    if (st.Length > i)
+                    {
+                        result = st[i];
+                        i++;
+                    }
+                    else
+                    {
+                        i = 0;
+                        result = st[0];
+                        i++;
+                    }
+                    while (result > 9)
+                        result = result / 10;
+                    arduinoBinarySequance += Convert.ToString(result, 2);
+                    return result.ToString();
+                }
+            }
+        }
+        private static string GetNextNumber(StreamReader sr)
+        {
+            string ch = " ";
+            while (ch == " ")
+            {
+                ch = char.ConvertFromUtf32(sr.Read());
+            }
+            int result = Int32.Parse(ch);
+            tableBinarySequance += Convert.ToString(result, 2); 
+            return ch;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string numbers = null;
+            Regex regex = new Regex(@"\d+");
+            MatchCollection matches = regex.Matches(textBox1.Text);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    numbers += match.Value;
+            }
+            string numbersBin = null;
+            foreach(char ch in numbers)
+            {
+                numbersBin += Convert.ToString(ch, 2);
+            }
+
+            float o = 0, l = 0;
+            for (int i = 0; i < numbersBin.Length; i++)
+            {
+                if (numbersBin[i] == '0')
+                    o++;
+                else if (numbersBin[i] == '1')
+                    l++;
+            }
+            label3.Text = (o / l).ToString();
+        }
+    }
+}
